ca0cc396c434ac1dfab4477471cd98c9ab03a199
Bruno Lebon <Bruno.Lebon@brunel.ac.uk>
2023-01-18 09:52:43 +0000

    Initial commit

---
+++ run/phase/qsub_OF10_submission.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -l
+
+# Parallel environment and number of procs
+#$ -pe mpi 40
+
+# Request 1 h of wallclock time
+#$ -l h_rt=1:0:0
+
+# Working directory
+#$ -cwd
+
+# Job name
+#$ -N OF10_phase
+
+# Email alerts
+#$ -m be
+#$ -M Bruno.Lebon@brunel.ac.uk
+
+#$ -P Test
+#$ -A Brunel_allocation
+
+
+pwd; hostname; date
+
+module purge
+module load userscripts
+module load beta-modules
+module load gcc-libs/10.2.0
+module load compilers/gnu/10.2.0
+module load mpi/openmpi/4.0.5/gnu-10.2.0
+module load cmake/3.21.1
+module load flex/2.5.39
+module load parallel
+module load dos2unix
+module load bison
+
+# source $HOME/OpenFOAM/OpenFOAM-9/etc/bashrc
+source /lustre/home/mmm0708/OpenFOAM/OpenFOAM-10/etc/bashrc
+
+# Load python3
+module load python3
+
+# Source tutorial run functions
+source $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+# Get application name
+export application=`getApplication`
+./Allrun
+
+postProcess -func 'cutPlaneSurface(funcName=zNormal, fields=(fi dT), point=(0. 0. 0.), normal=(0 0 1))' -latestTime
+
+touch $(basename $PWD).foam
+
+python3 convert_vtk.py
+python3 generate_pvd.py
+
+date
+
+#------------------------------------------------------------------------------
