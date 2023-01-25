4c10f8deaccd758b38ddf43f0263160dee5acba2
Bruno Lebon <Bruno.Lebon@brunel.ac.uk>
2023-01-25 11:30:39 +0000

    Add decomposeParDict entry change in PBS script

--- tutorials/multiphase/phaseFieldFoam/phase/qsub_OF10_submission.sh
+++ tutorials/multiphase/phaseFieldFoam/phase/qsub_OF10_submission.sh
@@ -29,12 +29,13 @@ module load gcc-libs/10.2.0
 module load compilers/gnu/10.2.0
 module load mpi/openmpi/4.0.5/gnu-10.2.0
 module load cmake/3.21.1
+module load boost/1.75.0
 module load flex/2.5.39
 module load parallel
 module load dos2unix
 module load bison
 
-# source $HOME/OpenFOAM/OpenFOAM-9/etc/bashrc
+# source $HOME/OpenFOAM/OpenFOAM-10/etc/bashrc
 source /lustre/home/mmm0708/OpenFOAM/OpenFOAM-10/etc/bashrc
 
 # Load python3
@@ -45,6 +46,11 @@ source $WM_PROJECT_DIR/bin/tools/RunFunctions
 
 # Get application name
 export application=`getApplication`
+
+# Set to number of procs
+foamDictionary system/decomposeParDict -entry numberOfSubdomains -set 40
+sed -i 's/(400 400 1)/(4000 4000 1)/g' system/blockMeshDict
+
 ./Allrun
 
 date
